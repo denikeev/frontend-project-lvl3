fde06ff8d15957144bfb5a17f7b32ed1d727ee23
denikeev <script.edf@gmail.com>
2022-06-17 12:26:24 +0300

    remove babel, webpack config in ES modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,12 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+export default {
   mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
       },
       {
         test: /\.css$/,
@@ -23,14 +16,6 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
-      // {
-      //   test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      //   use: 'url-loader?limit=10000',
-      // },
-      // {
-      //   test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-      //   use: 'file-loader',
-      // },
     ],
   },
   plugins: [
